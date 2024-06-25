4f56dee36323157a30b5fb30c30fb615276387de
Akram Rasikh <akram.rasikh@gmail.com>
2024-06-25 09:28:02 +0100

    useMasterAudioLoad refactored

--- hooks/useMasterAudioLoad.ts
+++ hooks/useMasterAudioLoad.ts
@@ -1,35 +1,40 @@
 import {useEffect, useState} from 'react';
 import Sound from 'react-native-sound';
 
-const useMasterAudioLoad = ({soundRef, url, masterSoundState}) => {
+const useMasterAudioLoad = ({soundRef, url}) => {
   const [soundState, setSoundState] = useState(null);
   useEffect(() => {
-    if (soundRef?.current?.isLoaded()) {
+    if (!url) {
       return;
     }
-    if (url) {
-      const soundInstance = new Sound(url, '', error => {
-        if (error) {
-          console.log('## Failed to load the sound', error);
-          return;
-        }
-        console.log(
-          'Duration in seconds: ' +
-            soundInstance.getDuration() +
-            ' number of channels: ' +
-            soundInstance.getNumberOfChannels(),
-        );
-        soundRef.current = soundInstance;
-        setSoundState(soundInstance);
-      });
 
-      return () => {
-        if (soundRef.current) {
-          soundRef.current.release();
-        }
-      };
+    // If already loaded, do not reload
+    if (soundRef?.current?.isLoaded()) {
+      setSoundState(soundRef.current);
+      return;
     }
-  }, [url, soundRef, masterSoundState]);
+
+    const soundInstance = new Sound(url, '', error => {
+      if (error) {
+        console.error('## Failed to load the sound', error);
+        return;
+      }
+      console.log(
+        'Duration in seconds: ' +
+          soundInstance.getDuration() +
+          ' number of channels: ' +
+          soundInstance.getNumberOfChannels(),
+      );
+      soundRef.current = soundInstance;
+      setSoundState(soundInstance);
+    });
+
+    return () => {
+      if (soundRef.current) {
+        soundRef.current.release();
+      }
+    };
+  }, [url]); // Only depend on the URL to prevent unnecessary reloads
 
   return soundState;
 };
