4bbf544877e9d6ce71c4ac14a9549de3d45a9aaa
Akram Rasikh <akram.rasikh@gmail.com>
2024-08-07 19:26:07 +0100

    remove siu

--- App.tsx
+++ App.tsx
@@ -396,9 +396,6 @@ function App(): React.JSX.Element {
 
   return (
     <SafeAreaView style={{backgroundColor: '#D3D3D3', minHeight: '100%'}}>
-      <View>
-        <Text>Siu</Text>
-      </View>
       {updatePromptState ? (
         <ToastMessage toastText={updatePromptState} />
       ) : null}
