78713cb57f9c514e78c5b1e1a6eee75422dd368f
Akram Rasikh <akram.rasikh@gmail.com>
2024-08-24 18:20:00 +0100

    clean up files on close

--- App.tsx
+++ App.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {AppState} from 'react-native';
+import RNFS from 'react-native-fs';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {enableScreens} from 'react-native-screens'; // Import this line
@@ -12,6 +14,35 @@ enableScreens();
 const Stack = createStackNavigator();
 
 function App(): React.JSX.Element {
+  useEffect(() => {
+    const subscription = AppState.addEventListener(
+      'change',
+      handleAppStateChange,
+    );
+
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+
+  const handleAppStateChange = nextAppState => {
+    if (nextAppState === 'background') {
+      clearTemporaryFiles();
+    }
+  };
+
+  const clearTemporaryFiles = async () => {
+    try {
+      const files = await RNFS.readDir(RNFS.TemporaryDirectoryPath);
+      files.forEach(async file => {
+        await RNFS.unlink(file.path);
+      });
+      console.log('## App Temporary files cleared');
+    } catch (err) {
+      console.error('## App Error clearing temporary files:', err);
+    }
+  };
+
   return (
     <DataProvider>
       <SafeAreaProvider>
