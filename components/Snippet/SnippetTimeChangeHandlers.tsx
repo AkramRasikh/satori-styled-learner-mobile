0281993f8bf40113aa3f6ce2d3e06f2e4fc000d5
Akram Rasikh <akram.rasikh@gmail.com>
2024-09-02 17:26:52 +0100

    MiniSnippetTimeChangeHandlers created

--- components/Snippet/SnippetTimeChangeHandlers.tsx
+++ components/Snippet/SnippetTimeChangeHandlers.tsx
@@ -1,4 +1,87 @@
-import {TouchableOpacity, Text, View} from 'react-native';
+import {TouchableOpacity, Text, View, Button} from 'react-native';
+
+export const MiniSnippetTimeChangeHandlers = ({
+  handleSetEarlierTime,
+  adjustableDuration,
+  handleSetDuration,
+  adjustableStartTime,
+  pauseSound,
+  playSound,
+  isPlaying,
+}) => {
+  const earlierTimeTitle = '⏪';
+  const laterTimeTitle = '⏩';
+  return (
+    <View
+      style={{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+      }}>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          alignItems: 'center',
+        }}>
+        <Text
+          style={{
+            marginHorizontal: 'auto',
+            fontStyle: 'italic',
+          }}>
+          {adjustableStartTime?.toFixed(2)} ➡{' '}
+          {(adjustableStartTime + adjustableDuration)?.toFixed(2)}
+        </Text>
+      </View>
+      <View
+        style={{
+          flex: 2,
+          marginHorizontal: 5,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <View style={{flex: 1}}>
+          <Button
+            onPress={() => handleSetEarlierTime(false)}
+            title={earlierTimeTitle}
+          />
+        </View>
+        <View style={{flex: 1}}>
+          <Button
+            onPress={() => handleSetEarlierTime(true)}
+            title={laterTimeTitle}
+          />
+        </View>
+        <View style={{flex: 1, alignSelf: 'center'}}>
+          <TouchableOpacity onPress={() => handleSetDuration(false)}>
+            <Text>-0.5</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={{flex: 1, alignSelf: 'center'}}>
+          <TouchableOpacity onPress={() => handleSetDuration(true)}>
+            <Text>+0.5</Text>
+          </TouchableOpacity>
+        </View>
+        <View
+          style={{
+            backgroundColor: isPlaying ? 'green' : 'red',
+            borderRadius: 5,
+            alignSelf: 'center',
+            padding: 5,
+          }}>
+          {true ? (
+            <TouchableOpacity onPress={pauseSound} disabled={!isPlaying}>
+              <Text>⏸️</Text>
+            </TouchableOpacity>
+          ) : (
+            <TouchableOpacity onPress={playSound} disabled={isPlaying}>
+              <Text>▶️</Text>
+            </TouchableOpacity>
+          )}
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const SnippetTimeChangeHandlers = ({
   handleSetEarlierTime,
