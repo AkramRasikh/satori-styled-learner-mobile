a723e7d7d24b58956ce51698fca08204b6bfbc17
Akram Rasikh <akram.rasikh@gmail.com>
2024-09-02 13:45:31 +0100

    removed redundant code

--- components/SoundSmallSize.tsx
+++ components/SoundSmallSize.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Button, TouchableOpacity, View} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Button, View} from 'react-native';
 
 const SoundSmallSize = ({
   soundRef,
