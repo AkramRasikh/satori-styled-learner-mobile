646e649095efb5375b34218bf4f265ac0a2eccb7
Akram Rasikh <akram.rasikh@gmail.com>
2024-09-02 17:33:51 +0100

    no progress added props

--- components/Progress.tsx
+++ components/Progress.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 import {View, Text} from 'react-native';
 import {ProgressBar} from 'react-native-paper';
 
-const ProgressBarComponent = ({progress, time, endTime}) => {
+const ProgressBarComponent = ({
+  progress,
+  time,
+  endTime,
+  noMargin,
+  noPadding,
+}) => {
   return (
-    <View style={{padding: 10, width: '100%'}}>
-      <ProgressBar progress={progress} style={{marginTop: 20, height: 10}} />
+    <View style={{padding: !noPadding ? 10 : 0, width: '100%'}}>
+      <ProgressBar
+        progress={progress}
+        style={{marginTop: !noMargin ? 20 : 0, height: 10}}
+      />
       <View
         style={{
           flexDirection: 'row',
