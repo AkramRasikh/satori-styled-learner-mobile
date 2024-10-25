13f0b412ac654db8f6a55aaf9099ee249812ed0e
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-25 16:35:29 +0100

    updated base files for build

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,17 @@
-const {getDefaultConfig} = require('expo/metro-config');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 
-const config = getDefaultConfig(__dirname);
+//SatoriNativeApp/node_modules/metro-file-map/src/watchers/NodeWatcher.js:82:12)
 
-const {transformer, resolver} = config;
-
-config.transformer = {
-  ...transformer,
-  babelTransformerPath: require.resolve('react-native-svg-transformer'),
+const config = {
+  resolver: {
+    assetExts: ['png', 'jpg', 'jpeg', 'svg', 'webp', 'gif'],
+  },
 };
 
-config.resolver = {
-  ...resolver,
-  assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
-  sourceExts: [...resolver.sourceExts, 'svg'],
-};
-
-module.exports = config;
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
