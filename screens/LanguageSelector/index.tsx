01ee638942eca84344e0d02c4a64a0499916cc97
Akram Rasikh <akram.rasikh@gmail.com>
2024-10-27 16:12:02 +0000

    basic lang selector screen made

--- screens/LanguageSelector/index.tsx
+++ screens/LanguageSelector/index.tsx
@@ -1,10 +1,61 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, TouchableOpacity, View} from 'react-native';
+
+const languageKey = {
+  japanese: 'japanese',
+  chinese: 'chinese',
+};
 
 const LanguageSelector = (): React.JSX.Element => {
+  const handleLanguageSelection = selectedLanguage => {
+    console.log('## handleLanguageSelection', {selectedLanguage});
+  };
   return (
     <View>
-      <Text>Language Selector Screen</Text>
+      <View
+        style={{
+          marginTop: 70,
+          alignSelf: 'center',
+          display: 'flex',
+          flexDirection: 'row',
+          gap: 10,
+        }}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: 'bold',
+            marginBottom: 20,
+          }}>
+          Select Language
+        </Text>
+      </View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignSelf: 'center',
+          alignItems: 'center',
+          gap: 10,
+        }}>
+        <View>
+          <TouchableOpacity
+            onPress={() => handleLanguageSelection(languageKey.chinese)}>
+            <Image
+              style={{height: 100, resizeMode: 'contain'}}
+              source={require(`../../assets/images/chinese-flag.png`)}
+            />
+          </TouchableOpacity>
+        </View>
+        <View>
+          <TouchableOpacity
+            onPress={() => handleLanguageSelection(languageKey.japanese)}>
+            <Image
+              style={{height: 100, resizeMode: 'contain'}}
+              source={require(`../../assets/images/japanese-flag.png`)}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
