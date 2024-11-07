4ae2c3d46e9817a1d762d246ef6f04660eaaece7
Akram Rasikh <akram.rasikh@gmail.com>
2024-11-07 12:14:36 +0000

    functional bottom section

--- components/TopicContentAudioSection.tsx
+++ components/TopicContentAudioSection.tsx
@@ -7,37 +7,47 @@ const TopicContentAudioSection = ({
   pauseSound,
   rewindSound,
   forwardSound,
-  // getTimeStamp,
-  // currentTimeState,
-  // soundDuration,
+  getTimeStamp,
+  currentTimeState,
+  soundDuration,
   jumpAudioValue = 5,
 }) => {
+  const progress =
+    soundDuration && currentTimeState ? currentTimeState / soundDuration : 0;
+
   return (
     <View
       style={{
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         padding: 10,
-        gap: 5,
+        gap: 10,
       }}>
-      <View
-        style={{
-          flex: 2,
-          marginVertical: 'auto',
-        }}>
-        <ProgressBar progress={0.5} color={MD3Colors.error50} />
+      <View>
+        <View
+          style={{
+            flex: 2,
+            marginVertical: 'auto',
+          }}>
+          <ProgressBar progress={progress} color={MD3Colors.error50} />
+        </View>
       </View>
+
       <View
         style={{
           gap: 5,
           display: 'flex',
           flexDirection: 'row',
+          justifyContent: 'space-between',
         }}>
-        <Button mode="contained" onPress={forwardSound} buttonColor="grey">
-          +{jumpAudioValue}
+        <Button mode="contained" onPress={getTimeStamp} buttonColor="grey">
+          ✂️
         </Button>
         <Button mode="contained" onPress={rewindSound} buttonColor="grey">
           -{jumpAudioValue}
+        </Button>
+        <Button mode="contained" onPress={forwardSound} buttonColor="grey">
+          +{jumpAudioValue}
         </Button>
         {isPlaying ? (
           <Button mode="contained" onPress={pauseSound} buttonColor={'green'}>
