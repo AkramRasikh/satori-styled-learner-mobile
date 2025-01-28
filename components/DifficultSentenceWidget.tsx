e8229f50b32d3a4df4d53617b92d2ce367c2d452
Akram Rasikh <akram.rasikh@gmail.com>
2025-01-28 18:42:39 +0000

    NestedwordsWithHyphens

--- components/DifficultSentenceWidget.tsx
+++ components/DifficultSentenceWidget.tsx
@@ -15,6 +15,58 @@ import {SRSTogglesQuickComprehensiveDiffSentencesWords} from './SRSToggles';
 import useHighlightWordToWordBank from '../hooks/useHighlightWordToWordBank';
 import {checkOverlap} from '../utils/check-word-overlap';
 
+const NestedwordsWithHyphens = ({
+  segment,
+  nestedCoordinates,
+  colorIndex,
+  getHexCode,
+}) => {
+  const [textWidth, setTextWidth] = useState(0);
+
+  return (
+    <View>
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          justifyContent: 'space-around',
+          maxWidth: textWidth,
+        }}>
+        {nestedCoordinates.map(item => {
+          return (
+            <Text
+              key={item}
+              style={{
+                color: getHexCode(item),
+                fontSize: 8,
+                fontWeight: 'bold',
+                display: 'flex',
+                flexDirection: 'row',
+                flexWrap: 'wrap',
+              }}>
+              ({item + 1})
+            </Text>
+          );
+        })}
+      </View>
+      <Text
+        style={{
+          ...segment.style,
+          fontSize: 20,
+          lineHeight: 24,
+          color: getHexCode(colorIndex),
+        }}
+        onLayout={event => {
+          const {width} = event.nativeEvent.layout;
+          setTextWidth(width);
+        }}>
+        {segment.text}
+      </Text>
+    </View>
+  );
+};
+
 const TopSection = ({thisSentenceIsLoading}) => {
   return (
     <>
@@ -321,33 +373,12 @@ const DifficultSentenceWidget = ({
 
   const getUIWithNestedWords = (nestedCoordinates, segment, colorIndex) => {
     return (
-      <View>
-        {nestedCoordinates.map(item => {
-          return (
-            <Text
-              key={item}
-              style={{
-                color: getHexCode(item),
-                fontSize: 10,
-                fontWeight: 'bold',
-                display: 'flex',
-                flexDirection: 'row',
-                flexWrap: 'wrap',
-              }}>
-              ({item + 1})
-            </Text>
-          );
-        })}
-        <Text
-          style={{
-            ...segment.style,
-            fontSize: 20,
-            lineHeight: 24,
-            color: getHexCode(colorIndex),
-          }}>
-          {segment.text}
-        </Text>
-      </View>
+      <NestedwordsWithHyphens
+        segment={segment}
+        nestedCoordinates={nestedCoordinates}
+        colorIndex={colorIndex}
+        getHexCode={getHexCode}
+      />
     );
   };
 
