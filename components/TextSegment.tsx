110c3f1ee680e102eb88ab8ba4cd419e1e597f23
Akram Rasikh <akram.rasikh@gmail.com>
2025-01-29 15:20:05 +0000

    little refator

--- components/TextSegment.tsx
+++ components/TextSegment.tsx
@@ -1,27 +1,23 @@
 import React, {Text} from 'react-native';
 
-const TextSegment = ({textSegments}) => {
-  return (
-    <Text>
-      {textSegments.map((segment, index) => {
-        return (
-          <Text
-            key={index}
-            id={segment.id}
-            selectable={true}
-            style={[
-              segment.style,
-              {
-                fontSize: 20,
-                lineHeight: 24,
-              },
-            ]}>
-            {segment.text}
-          </Text>
-        );
-      })}
-    </Text>
-  );
-};
+const TextSegment = ({textSegments}) => (
+  <Text>
+    {textSegments.map((segment, index) => (
+      <Text
+        key={index}
+        id={segment.id}
+        selectable={true}
+        style={[
+          segment.style,
+          {
+            fontSize: 20,
+            lineHeight: 24,
+          },
+        ]}>
+        {segment.text}
+      </Text>
+    ))}
+  </Text>
+);
 
 export default TextSegment;
