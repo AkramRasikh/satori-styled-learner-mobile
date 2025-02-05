7920a2861ecc3e4058d1c7aa7696fd0341776f79
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-05 00:24:44 +0000

    topics refactored

--- components/Topics.tsx
+++ components/Topics.tsx
@@ -3,6 +3,71 @@ import TopicTitleButton from './TopicTitleButton';
 import {View} from 'react-native';
 import {FAB} from 'react-native-paper';
 
+const TopicSectionContainer = ({
+  label,
+  onPress,
+  topicArr,
+  handleOnPress,
+  selectedGeneralTopicState,
+  condition,
+}) => {
+  const mappedView = topicData => {
+    const title = topicData.title;
+    const isCore = topicData.isCore;
+    const isDue = topicData.isDue;
+    const futureReview = topicData.hasFutureReview;
+    const isYoutube = topicData.isYoutube;
+    const isGeneral = topicData.isGeneral;
+    const hasAudio = topicData?.hasAudio;
+    const isMediaContent = topicData?.isMediaContent;
+    const isSelectedSubsetOfGeneralTopic =
+      topicData?.generalTopic === selectedGeneralTopicState;
+    const showGeneralTopics = isGeneral && !selectedGeneralTopicState;
+
+    const generalTopicMode = !isGeneral && isSelectedSubsetOfGeneralTopic;
+
+    if (showGeneralTopics || generalTopicMode) {
+      return (
+        <TopicTitleButton
+          key={title}
+          onPress={() => handleOnPress(title)}
+          testID={title}
+          isDue={isDue}
+          futureReview={futureReview}
+          title={title}
+          isCore={isCore}
+          isYoutube={isYoutube}
+          isGeneral={isGeneral}
+          hasAudio={hasAudio}
+          isNetflix={isMediaContent && !isYoutube}
+        />
+      );
+    }
+
+    return null;
+  };
+  return (
+    <View>
+      <FAB
+        icon={condition ? 'folder-open' : 'plus'}
+        label={label}
+        onPress={onPress}
+      />
+      {condition && (
+        <View
+          style={{
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            gap: 10,
+            paddingTop: 10,
+          }}>
+          {topicArr.map(mappedView)}
+        </View>
+      )}
+    </View>
+  );
+};
+
 const Topics = ({
   allTopicsMetaDataState,
   selectedGeneralTopicState,
@@ -46,42 +111,6 @@ const Topics = ({
     }
   };
 
-  const mappedView = topicData => {
-    const title = topicData.title;
-    const isCore = topicData.isCore;
-    const isDue = topicData.isDue;
-    const futureReview = topicData.hasFutureReview;
-    const isYoutube = topicData.isYoutube;
-    const isGeneral = topicData.isGeneral;
-    const hasAudio = topicData?.hasAudio;
-    const isMediaContent = topicData?.isMediaContent;
-    const isSelectedSubsetOfGeneralTopic =
-      topicData?.generalTopic === selectedGeneralTopicState;
-    const showGeneralTopics = isGeneral && !selectedGeneralTopicState;
-
-    const generalTopicMode = !isGeneral && isSelectedSubsetOfGeneralTopic;
-
-    if (showGeneralTopics || generalTopicMode) {
-      return (
-        <TopicTitleButton
-          key={title}
-          onPress={() => handleOnPress(title)}
-          testID={title}
-          isDue={isDue}
-          futureReview={futureReview}
-          title={title}
-          isCore={isCore}
-          isYoutube={isYoutube}
-          isGeneral={isGeneral}
-          hasAudio={hasAudio}
-          isNetflix={isMediaContent && !isYoutube}
-        />
-      );
-    }
-
-    return null;
-  };
-
   let mediaGeneralTopicCount = 0;
   let contentGeneralTopicCount = 0;
   const mediaContent = [];
@@ -106,42 +135,22 @@ const Topics = ({
 
   return (
     <View style={{gap: 10}}>
-      <View>
-        <FAB
-          icon={isContent ? 'folder-open' : 'plus'}
-          label={`Content (${contentGeneralTopicCount}/${standardContent.length})`}
-          onPress={handleShowContent}
-        />
-        {isContent && (
-          <View
-            style={{
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              gap: 10,
-              paddingTop: 10,
-            }}>
-            {standardContent.map(mappedView)}
-          </View>
-        )}
-      </View>
-      <View>
-        <FAB
-          icon={isMedia ? 'folder-open' : 'plus'}
-          label={`Media (${mediaGeneralTopicCount}/${mediaContent.length})`}
-          onPress={handleShowMedia}
-        />
-        {isMedia && (
-          <View
-            style={{
-              flexDirection: 'row',
-              flexWrap: 'wrap',
-              gap: 10,
-              paddingTop: 10,
-            }}>
-            {mediaContent.map(mappedView)}
-          </View>
-        )}
-      </View>
+      <TopicSectionContainer
+        label={`Content (${contentGeneralTopicCount}/${standardContent.length})`}
+        onPress={handleShowContent}
+        topicArr={standardContent}
+        handleOnPress={handleOnPress}
+        selectedGeneralTopicState={selectedGeneralTopicState}
+        condition={isContent}
+      />
+      <TopicSectionContainer
+        label={`Media (${mediaGeneralTopicCount}/${mediaContent.length})`}
+        onPress={handleShowMedia}
+        topicArr={mediaContent}
+        handleOnPress={handleOnPress}
+        selectedGeneralTopicState={selectedGeneralTopicState}
+        condition={isMedia}
+      />
     </View>
   );
 };
