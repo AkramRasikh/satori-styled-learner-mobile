15aae3387baa5746fa50f04d5218150eb194b32f
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-06 21:19:38 +0000

    showing laoding spinner in place of collapsible element

--- components/FlashcardsWordsSection.tsx
+++ components/FlashcardsWordsSection.tsx
@@ -3,6 +3,7 @@ import {Animated, Dimensions, Text, TouchableOpacity, View} from 'react-native';
 import AnimatedWordModal from './WordModal';
 import {SRSTogglesQuickComprehensive} from './SRSToggles';
 import {
+  ActivityIndicator,
   Button,
   Card,
   Divider,
@@ -44,6 +45,7 @@ const Container = ({
   handleCloseModal,
   handleExpandWordArray,
 }) => {
+  const [isCollapsingState, setIsCollapsingState] = useState(false);
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const scaleAnim = useRef(new Animated.Value(0.8)).current;
 
@@ -65,91 +67,109 @@ const Container = ({
 
   const wordListText = listTextNumber + baseForm;
 
+  const collapseAnimationWithState = async () => {
+    setIsCollapsingState(true);
+    await collapseAnimation();
+  };
+
   const handleDeleteWordWithAnimation = async () => {
+    setIsCollapsingState(true);
     await collapseAnimation();
     handleDeleteWord(wordData);
   };
   return (
-    <AnimationContainer fadeAnim={fadeAnim} scaleAnim={scaleAnim}>
-      <View>
-        <Card
-          style={{
-            padding: 5,
-            width: isSelectedWord ? width * 0.9 : 'auto',
-            backgroundColor: cardReviewButNotDue
-              ? MD2Colors.blue100
-              : isCardDue && MD2Colors.red100,
-          }}>
-          <TouchableOpacity
-            onPress={() => setSelectedDueCardState(wordData)}
-            style={{
-              alignSelf: 'flex-start',
-              marginVertical: 5,
-              width: '100%',
-              justifyContent: 'space-between',
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
-            <Text
-              style={{
-                fontSize: 24,
-                flexWrap: 'wrap',
-              }}>
-              {wordListText}
-              {
-                <Icon
-                  source={freshCard ? 'new-box' : ''}
-                  size={24}
-                  color={MD2Colors.green500}
-                />
-              }
-            </Text>
-            {isSelectedWord && (
-              <IconButton
-                icon="close"
-                onPress={handleCloseModal}
-                mode="outlined"
-                iconColor={'white'}
-                containerColor={MD3Colors.error50}
-              />
-            )}
-          </TouchableOpacity>
-          <Divider bold />
-          {!isSelectedWord && (
-            <SRSTogglesQuickComprehensive
-              reviewData={wordData.reviewData}
-              id={wordId}
-              baseForm={baseForm}
-              deleteWord={handleDeleteWordWithAnimation}
-              collapseAnimation={collapseAnimation}
-            />
-          )}
-          {isSelectedWord && (
-            <AnimatedWordModal
-              visible={wordData}
-              onClose={handleCloseModal}
-              deleteWord={handleDeleteWordWithAnimation}
-              collapseAnimation={collapseAnimation}
-            />
-          )}
-        </Card>
-      </View>
-      {moreToLoad && (
+    <>
+      {isCollapsingState && (
         <View
           style={{
+            margin: 10,
+            alignItems: 'center',
             width: '100%',
-            paddingBottom: 30,
           }}>
-          <Button
-            onPress={handleExpandWordArray}
-            icon="refresh"
-            mode="outlined">
-            See More
-          </Button>
+          <ActivityIndicator size="large" />
         </View>
       )}
-    </AnimationContainer>
+      <AnimationContainer fadeAnim={fadeAnim} scaleAnim={scaleAnim}>
+        <View>
+          <Card
+            style={{
+              padding: 5,
+              width: isSelectedWord ? width * 0.9 : 'auto',
+              backgroundColor: cardReviewButNotDue
+                ? MD2Colors.blue100
+                : isCardDue && MD2Colors.red100,
+            }}>
+            <TouchableOpacity
+              onPress={() => setSelectedDueCardState(wordData)}
+              style={{
+                alignSelf: 'flex-start',
+                marginVertical: 5,
+                width: '100%',
+                justifyContent: 'space-between',
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+              }}>
+              <Text
+                style={{
+                  fontSize: 24,
+                  flexWrap: 'wrap',
+                }}>
+                {wordListText}
+                {
+                  <Icon
+                    source={freshCard ? 'new-box' : ''}
+                    size={24}
+                    color={MD2Colors.green500}
+                  />
+                }
+              </Text>
+              {isSelectedWord && (
+                <IconButton
+                  icon="close"
+                  onPress={handleCloseModal}
+                  mode="outlined"
+                  iconColor={'white'}
+                  containerColor={MD3Colors.error50}
+                />
+              )}
+            </TouchableOpacity>
+            <Divider bold />
+            {!isSelectedWord && (
+              <SRSTogglesQuickComprehensive
+                reviewData={wordData.reviewData}
+                id={wordId}
+                baseForm={baseForm}
+                deleteWord={handleDeleteWordWithAnimation}
+                collapseAnimation={collapseAnimationWithState}
+              />
+            )}
+            {isSelectedWord && (
+              <AnimatedWordModal
+                visible={wordData}
+                onClose={handleCloseModal}
+                deleteWord={handleDeleteWordWithAnimation}
+                collapseAnimation={collapseAnimationWithState}
+              />
+            )}
+          </Card>
+        </View>
+        {moreToLoad && (
+          <View
+            style={{
+              width: '100%',
+              paddingBottom: 30,
+            }}>
+            <Button
+              onPress={handleExpandWordArray}
+              icon="refresh"
+              mode="outlined">
+              See More
+            </Button>
+          </View>
+        )}
+      </AnimationContainer>
+    </>
   );
 };
 
