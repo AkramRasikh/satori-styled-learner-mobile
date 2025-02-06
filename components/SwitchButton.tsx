bab112a05ff08564603403bffdb776e552059dcf
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-06 00:10:20 +0000

    switch button

--- components/SwitchButton.tsx
+++ components/SwitchButton.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {View, StyleSheet, TouchableOpacity} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {Switch} from 'react-native-paper';
 
 const SwitchButton = ({isOn, setIsOn}) => {
   const toggleSwitch = () => {
@@ -7,20 +8,9 @@ const SwitchButton = ({isOn, setIsOn}) => {
   };
 
   return (
-    <TouchableOpacity style={styles.switchContainer} onPress={toggleSwitch}>
-      <View
-        style={[
-          styles.switchBackground,
-          isOn ? styles.switchOn : styles.switchOff,
-        ]}>
-        <View
-          style={[
-            styles.switchThumb,
-            isOn ? styles.switchThumbOn : styles.switchThumbOff,
-          ]}
-        />
-      </View>
-    </TouchableOpacity>
+    <View style={styles.switchContainer}>
+      <Switch value={isOn} onValueChange={toggleSwitch} />
+    </View>
   );
 };
 
@@ -28,33 +18,7 @@ const styles = StyleSheet.create({
   switchContainer: {
     padding: 10,
     margin: 'auto',
-    cursor: 'pointer', // Note: 'cursor' is a web-specific property. It can be omitted in React Native.
-  },
-  switchBackground: {
-    width: 50,
-    height: 25,
-    borderRadius: 25,
-    position: 'relative',
-  },
-  switchOn: {
-    backgroundColor: 'green',
-  },
-  switchOff: {
-    backgroundColor: 'red',
-  },
-  switchThumb: {
-    width: 23,
-    height: 23,
-    backgroundColor: 'white',
-    borderRadius: 15,
-    position: 'absolute',
-    top: 1,
-  },
-  switchThumbOn: {
-    left: 26,
-  },
-  switchThumbOff: {
-    left: 1,
+    cursor: 'pointer',
   },
 });
 
