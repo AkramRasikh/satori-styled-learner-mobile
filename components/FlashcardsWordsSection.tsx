a24ccd840faa0172d55b8491a62edd2a8929551f
Akram Rasikh <akram.rasikh@gmail.com>
2025-02-06 14:01:08 +0000

    tided up word

--- components/FlashcardsWordsSection.tsx
+++ components/FlashcardsWordsSection.tsx
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import {Button, Dimensions, Text, TouchableOpacity, View} from 'react-native';
+import {Dimensions, Text, TouchableOpacity, View} from 'react-native';
 import AnimatedWordModal from './WordModal';
 import {SRSTogglesQuickComprehensive} from './SRSToggles';
+import {Button, Divider, Icon, MD2Colors} from 'react-native-paper';
 
 export const FlashCardsSectionContainer = ({
   handleDeleteWordFlashCard,
@@ -44,7 +45,6 @@ const FlashcardsWordsSection = ({
         gap: 5,
       }}>
       {dueCardsState?.map((wordData, index) => {
-        const isLastEl = dueCardsState.length === index + 1;
         const listTextNumber = index + 1 + ') ';
         const wordId = wordData.id;
         const isSelectedWord = selectedDueCardState?.id === wordId;
@@ -56,57 +56,76 @@ const FlashcardsWordsSection = ({
 
         const moreToLoad = sliceArrState === index + 1 && !isLastInTotalOrder;
 
+        const wordListText = listTextNumber + baseForm;
         return (
-          <View
-            key={wordId}
-            style={{
-              paddingBottom: isLastEl ? 20 : 0,
-            }}>
-            <View
-              style={{
-                borderBlockColor: 'black',
-                borderWidth: 2,
-                padding: 5,
-                borderRadius: 5,
-                width: isSelectedWord ? width * 0.9 : 'auto',
-                backgroundColor: cardReviewButNotDue
-                  ? '#ADD8E6'
-                  : isCardDue && 'pink',
-              }}>
-              <TouchableOpacity
+          <React.Fragment key={wordId}>
+            <View>
+              <View
                 style={{
-                  borderBottomWidth: 1,
-                  paddingBottom: 5,
-                }}
-                onPress={() => setSelectedDueCardState(wordData)}>
-                <Text
+                  borderBlockColor: 'black',
+                  borderWidth: 1,
+                  padding: 5,
+                  borderRadius: 5,
+                  width: isSelectedWord ? width * 0.9 : 'auto',
+                  backgroundColor: cardReviewButNotDue
+                    ? MD2Colors.blue100
+                    : isCardDue && MD2Colors.red100,
+                }}>
+                <TouchableOpacity
+                  onPress={() => setSelectedDueCardState(wordData)}
                   style={{
-                    fontSize: 24,
+                    alignSelf: 'flex-start',
+                    marginVertical: 5,
+                    width: '100%',
                   }}>
-                  {listTextNumber}
-                  {baseForm} {freshCard ? '🆕' : ''}
-                </Text>
-              </TouchableOpacity>
-              {!isSelectedWord && (
-                <SRSTogglesQuickComprehensive
-                  reviewData={wordData.reviewData}
-                  id={wordId}
-                  baseForm={baseForm}
-                  deleteWord={() => handleDeleteWord(wordData)}
-                />
-              )}
-              {isSelectedWord && (
-                <AnimatedWordModal
-                  visible={wordData}
-                  onClose={handleCloseModal}
-                  deleteWord={() => handleDeleteWord(wordData)}
-                />
-              )}
+                  <Text
+                    style={{
+                      fontSize: 24,
+                      flexWrap: 'wrap',
+                    }}>
+                    {wordListText}
+                    {
+                      <Icon
+                        source={freshCard ? 'new-box' : ''}
+                        size={24}
+                        color={MD2Colors.green500}
+                      />
+                    }
+                  </Text>
+                </TouchableOpacity>
+                <Divider bold />
+                {!isSelectedWord && (
+                  <SRSTogglesQuickComprehensive
+                    reviewData={wordData.reviewData}
+                    id={wordId}
+                    baseForm={baseForm}
+                    deleteWord={() => handleDeleteWord(wordData)}
+                  />
+                )}
+                {isSelectedWord && (
+                  <AnimatedWordModal
+                    visible={wordData}
+                    onClose={handleCloseModal}
+                    deleteWord={() => handleDeleteWord(wordData)}
+                  />
+                )}
+              </View>
             </View>
             {moreToLoad && (
-              <Button onPress={handleExpandWordArray} title="See More" />
+              <View
+                style={{
+                  width: '100%',
+                  paddingBottom: 30,
+                }}>
+                <Button
+                  onPress={handleExpandWordArray}
+                  icon="refresh"
+                  mode="outlined">
+                  See More
+                </Button>
+              </View>
             )}
-          </View>
+          </React.Fragment>
         );
       })}
     </View>
