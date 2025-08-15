80422434251acfdcd1c5067044516dedf4f63183
Akram Rasikh <akram.rasikh@gmail.com>
2025-08-15 11:34:39 +0100

    loading widget

--- components/HighlightTextHover.tsx
+++ components/HighlightTextHover.tsx
@@ -1,11 +1,22 @@
+import {useState} from 'react';
 import React, {TouchableOpacity, View} from 'react-native';
-import {Divider, Text} from 'react-native-paper';
+import {ActivityIndicator, Divider, Text} from 'react-native-paper';
 
 const HighlightTextHover = ({
   getHighlightedText,
   handleQuickGoogleTranslate,
   handleClose,
 }) => {
+  const [isLoadingState, setIsLoadingState] = useState(false);
+  const quickTrnaslate = async () => {
+    try {
+      setIsLoadingState(true);
+      await handleQuickGoogleTranslate(getHighlightedText());
+    } catch (error) {
+    } finally {
+      setIsLoadingState(false);
+    }
+  };
   return (
     <View
       style={{
@@ -31,11 +42,19 @@ const HighlightTextHover = ({
           flexDirection: 'row',
           alignSelf: 'center',
           gap: 10,
+          opacity: isLoadingState ? 0.5 : 1,
         }}>
-        <TouchableOpacity
-          onPress={async () =>
-            await handleQuickGoogleTranslate(getHighlightedText())
-          }>
+        {isLoadingState && (
+          <ActivityIndicator
+            style={{
+              position: 'absolute',
+              alignSelf: 'center',
+              bottom: '50%',
+              zIndex: 100,
+            }}
+          />
+        )}
+        <TouchableOpacity onPress={quickTrnaslate}>
           <Text>💨</Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => handleClose()}>
